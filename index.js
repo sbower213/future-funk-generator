d792493d13a254a1c377aa2fcc8b648d462c83f0
Kevin Jordan <kjordan777@gmail.com>
2018-04-08 01:01:26 -0400

    updated index and package

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var fs = require('fs');
 var MarkovChain = require('markovchain');
+var tracery = require('tracery');
 var app = express();
 var PORT = 3000;
 
 var songArray = fs.readFileSync('./futurefunksonglist.txt').toString().split(/\s+/);
+var nameGrammar = JSON.parse(fs.readFileSync('./namegrammar.json').toString());
 //console.log(songArray);
 
 //var titleMarkov = new MarkovChain(fs.readFileSync('./futurefunksonglist.txt', 'utf8'))
@@ -17,14 +19,19 @@ var songArray = fs.readFileSync('./futurefunksonglist.txt').toString().split(/\s
   return wordList[~~(Math.random()*wordList.length)];
 }*/
 
-app.get("/", function(req,res){
-	//console.log(titleMarkov.start(getChains).end(5).process());
-	var songName = "";
+var getSongName = function() {
+    var songName = "";
 	var songLength = 1 + Math.floor(Math.random() * 6);
 	for(var i=0; i < songLength; i++){
 		songName += songArray[Math.floor(Math.random() * songArray.length)] + " ";
 	}
-	res.send(songName);
+    return songName;
+}
+
+app.get("/", function(req,res){
+	//console.log(titleMarkov.start(getChains).end(5).process());
+	var grammar = tracery.createGrammar(nameGrammar);
+	res.send(getSongName());
 });
 
 app.listen(PORT, function() {
